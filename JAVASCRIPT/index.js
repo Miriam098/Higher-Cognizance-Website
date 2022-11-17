d83eae14be427bd7296a1cec4ef657abe3c972a1
Miriam Olivier <miriamolivier098@gmail.com>
2022-11-17 16:30:04 +0200

    Added new features

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,6 +1,27 @@
-let mainNav = document.getElementById("nav-ul");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+let mainNav = document.getElementById(`nav-ul`);
+let navBarToggle = document.getElementById(`js-navbar-toggle`);
+let modal = document.getElementById(`simpleModal`);
+let modalBtn = document.getElementById(`modalBtn`);
+let closeBtn = document.getElementsByClassName(`closeBtn`)[0];
 
-navBarToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
+navBarToggle.addEventListener(`click`, function () {
+  mainNav.classList.toggle(`click`);
 });
+
+modalBtn.addEventListener(`click`, openModal);
+closeBtn.addEventListener(`click`, closeModal);
+window.addEventListener(`click`, clickOutside);
+
+function openModal() {
+  modal.style.display = `block`;
+}
+
+function closeModal() {
+  modal.style.display = `none`;
+}
+
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = `none`;
+  }
+}
